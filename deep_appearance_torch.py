a8e32642f399d56c0bf1abc006cbbc1bdc1f8a6e
jeff <sc18j3j@leeds.ac.uk>
2020-08-05 19:16:55 +0100

    refactor: deep sort tracker has its own file

--- deep_appearance_torch.py
+++ deep_appearance_torch.py
@@ -1,14 +1,7 @@
-import numpy as np
-
 import torch
-import torchvision
 import torchvision.transforms as transforms
 
-from PIL import Image
-
-from full_body_net.full_body_net import FullBodyNet
-from sort import Sort
-
+from fullbodynet.full_body_net import FullBodyNet
 
 
 class DeepAppearance:
@@ -61,34 +54,3 @@ class DeepAppearance:
                     embeddings_out.append(embedding)
             
         return embeddings_out
-
-
-
-class DeepSort():
-    """
-    wrapper class for Sort using deep appearance above
-    """
-    def __init__(self, max_age=30, min_hits=3):
-        self.deep_appearance = DeepAppearance()
-        self.sort = Sort(max_age, min_hits)
-    
-    def update(self, frame, dets):
-        frame = np.flip(frame, axis=2)
-        H,W,C = frame.shape
-        images = []
-
-        for detection in dets:
-            x1 = max(int(detection[0]), 0)
-            y1 = max(int(detection[1]), 0)
-            x2 = min(int(detection[2]), W)
-            y2 = min(int(detection[3]), H)
-            image = frame[y1:y2, x1:x2, :]
-            image = Image.fromarray(image)
-            images.append(image)
-        
-        if len(images) > 0:
-            embs = self.deep_appearance.predict_embeddings(images)
-        else:
-            embs = []
-        
-        return self.sort.update(dets, embs)
---
+++ deepsort.py
@@ -0,0 +1,34 @@
+import numpy as np
+from PIL import Image
+
+from sort import Sort
+
+
+class DeepSort():
+    """
+    wrapper class for Sort using deep appearance above
+    """
+    def __init__(self, max_age=30, min_hits=3):
+        self.deep_appearance = DeepAppearance()
+        self.sort = Sort(max_age, min_hits)
+    
+    def update(self, frame, dets):
+        frame = np.flip(frame, axis=2)
+        H,W,C = frame.shape
+        images = []
+
+        for detection in dets:
+            x1 = max(int(detection[0]), 0)
+            y1 = max(int(detection[1]), 0)
+            x2 = min(int(detection[2]), W)
+            y2 = min(int(detection[3]), H)
+            image = frame[y1:y2, x1:x2, :]
+            image = Image.fromarray(image)
+            images.append(image)
+        
+        if len(images) > 0:
+            embs = self.deep_appearance.predict_embeddings(images)
+        else:
+            embs = []
+        
+        return self.sort.update(dets, embs)
